d68abf9f4720d114990ab5817490fa90e2558e6f
RJ101-RG <rishi.juneja@rategain.com>
2025-07-11 19:19:15 +0530

    Create App.tsx

---
+++ src/components/src/src/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { AvatarPreview } from './components/AvatarPreview';
+
+function App() {
+  return (
+    <div className="p-6">
+      <AvatarPreview originalVideo={null} processedData={{}} />
+    </div>
+  );
+}
+
+export default App;
